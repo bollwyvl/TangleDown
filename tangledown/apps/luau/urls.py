c2d44c96d5330e6a5d158662aa8e122dd3da76e5
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2011-12-02 17:54:55 -0500

    created static stuff

--- tangledown/apps/luau/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns("luau.views",
-    url(r'^$', 'show_page'),
-    url(r'^(?P<page_slug>[^/]*)/$', 'show_page'),
-    url(r'^(?P<page_slug>[^/]*)/edit/$', 'edit_page'),
-)
--- tangledown/util/shortcuts.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-def context_response(req, *args, **kwargs):
-    kwargs['context_instance'] = RequestContext(req)
-    return render_to_response(*args, **kwargs)
